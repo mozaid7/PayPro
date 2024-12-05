b8ae5b306f3482f8fc141983c95e224c2697e266
Mohammed Zaid <mohammedzee98@gmail.com>
2024-12-06 03:09:11 +0530

    value prop added

--- packages/ui/src/TextInput.tsx
+++ packages/ui/src/TextInput.tsx
@@ -3,14 +3,18 @@
 export const TextInput = ({
     placeholder,
     onChange,
-    label
+    label,
+    value,
 }: {
     placeholder: string;
     onChange: (value: string) => void;
     label: string;
+    value: string; // Define value type
 }) => {
     return <div className="pt-2">
         <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-        <input onChange={(e) => onChange(e.target.value)} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} />
+        <input onChange={(e) => onChange(e.target.value)} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder}
+        value={value} // Bind value to input 
+        />
     </div>
 }
