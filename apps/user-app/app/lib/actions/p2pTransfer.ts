1f5cd027810ac50403c31501e95fc27e1af48bc0
Mohammed Zaid <mohammedzee98@gmail.com>
2025-01-28 00:02:39 +0530

    p2p action logic

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -41,15 +41,6 @@ export async function p2pTransfer(to: string, amount: number) {
             where: { userId: toUser.id },
             data: { amount: { increment: amount } },
           });
-
-        //   await tx.p2pTransfer.create({
-        //     data: {
-        //         fromUserId: Number(from),
-        //         toUserId: toUser.id,
-        //         amount,
-        //         timestamp: new Date()
-        //     }
-        //   })
-          // locking
+    
     });
 }
