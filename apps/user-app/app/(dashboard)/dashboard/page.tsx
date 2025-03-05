cec68113236e35d25dbf96cae3d1ca34cfccfe54
Mohammed Zaid <mohammedzee98@gmail.com>
2025-03-05 22:42:52 +0530

    image on dashboard added

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
 
-export default function() {
-    return <div>
-        Dashboard
-    </div>
+export default function Dashboard() {
+    return (
+        <div className="flex items-center justify-center w-screen h-screen bg-[#ebe6e6]">
+            <div className="relative w-[90%] h-[90%] rounded-2xl overflow-hidden shadow-lg">
+                <Image 
+                    src="/images/payimage.jpg" 
+                    alt="Dashboard" 
+                    fill 
+                    className="object-cover"
+                />
+            </div>
+        </div>
+    );
 }
