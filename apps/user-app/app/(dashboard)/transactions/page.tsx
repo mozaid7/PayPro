154a4514efef69dbd8e28ae0aeb898a75b13b3ac
Mohammed Zaid <mohammedzee98@gmail.com>
2025-03-06 16:39:05 +0530

    transaction image added

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
 
-export default function() {
-    return <div>
-        Transactions
-    </div>
+export default function Transactions() {
+    return (
+        <div className="flex items-center justify-center w-screen h-screen bg-[#ebe6e6]">
+            <div className="relative w-[90%] h-[90%] rounded-2xl overflow-hidden shadow-lg">
+                <Image 
+                    src="/images/txn1.jpg" 
+                    alt="Transactions" 
+                    fill 
+                    className="object-cover"
+                />
+            </div>
+        </div>
+    );
 }
